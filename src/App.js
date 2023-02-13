a9ae1d059f5193fd86cbc74e9e7b952399b84a94
Francis Maina <sebastianfrancix@gmail.com>
2023-02-13 12:59:01 +0300

    added the details section grid template

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+
+import { Grid } from "@material-ui/core";
+
+import Details from "./Components/Details/Details";
 
 const App = () => {
   return (
     <div>
-      <h2>App</h2>
-    </div>
-  )
-}
+      <Grid
+        container
+        spacing={0}
+        alignItems="center"
+        justify="center"
+        style={{ height: "100vh" }}
+      >
+        <Grid item xs={12} sm={4}>
+          <Details />
+        </Grid>
 
-export default App
+        <Grid item xs={12} sm={4}>
+          Main
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <Details />
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default App;
