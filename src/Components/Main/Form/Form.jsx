f41026e1104625dcd6a64b4529eb3ab0fcda45b8
Francis Maina <sebastianfrancix@gmail.com>
2023-02-13 13:51:56 +0300

    added material-ui  styling for the Form component

---
+++ src/Components/Main/Form/Form.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {
+  TextField,
+  Typography,
+  Grid,
+  Button,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from "@material-ui/core";
+
+const Form = () => {
+  return <div></div>;
+};
+
+export default Form;
