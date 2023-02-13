4076e5639145d0ad828f9501c4f8f8a2ca205c90
Francis Maina <sebastianfrancix@gmail.com>
2023-02-13 13:22:48 +0300

    added the Main Component

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  Typography,
+  Grid,
+  Divider,
+} from "@material-ui/core";
+
+const Main = () => {
+  return (
+    <Card className={classes.root}>
+      <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
+    </Card>
+  );
+};
+
+export default Main;
