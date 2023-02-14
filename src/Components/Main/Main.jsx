3ac93192e8ae42fd059eba279c7336ff8eeccccb
Francis Maina <sebastianfrancix@gmail.com>
2023-02-14 17:44:22 +0300

    added the List Component and imported it to our Main component

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -10,6 +10,7 @@ import {
 
 import useStyles from "./styles";
 import Form from "./Form/Form";
+import List from "./List/List";
 
 const Main = () => {
   const classes = useStyles();
@@ -39,7 +40,8 @@ const Main = () => {
       <CardContent className={classes.cardContent}>
         <Grid container spacing={2}>
           <Grid item xs={12}>
-            {/* List */}
+            {/* List Component*/}
+            <List />
           </Grid>
         </Grid>
       </CardContent>
