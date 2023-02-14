b5749f23a4f2168fb9fb174be6b1e65f3c4d989e
Francis Maina <sebastianfrancix@gmail.com>
2023-02-14 22:06:24 +0300

    added logic for our context reducer

--- src/context/contextReducer.js
+++ src/context/contextReducer.js
@@ -1,5 +1,23 @@
 // Reducer => is a function that takes in the old state and an action and returns a new state
 
-const contextReducer = (state, action) => {};
+const contextReducer = (state, action) => {
+  let transactions;
+
+  switch (action.type) {
+    case "DELETE_TRANSACTION":
+      // filter and delete transaction based on their id
+      transactions = state.filter((t) => t.id !== action.payload);
+
+      return transactions;
+
+    case "ADD_TRANSACTION":
+      transactions = [action.payload, ...state];
+
+      return transactions;
+
+    default:
+      break;
+  }
+};
 
 export default contextReducer;
