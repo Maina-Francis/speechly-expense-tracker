d8b37523d0a8fc560691a935ace6992046d5c83a
Francis Maina <sebastianfrancix@gmail.com>
2023-02-17 02:12:21 +0300

    added push to talk button from speechly at the bottom of the page

--- src/App.js
+++ src/App.js
@@ -5,34 +5,44 @@ import { Grid } from "@material-ui/core";
 import Details from "./Components/Details/Details";
 import Main from "./Components/Main/Main";
 
+import {
+  PushToTalkButton,
+  PushToTalkButtonContainer,
+} from "@speechly/react-ui";
+
 import useStyles from "./styles";
 
 const App = () => {
   const classes = useStyles();
 
   return (
-    <div>
+    <>
       <Grid
         className={classes.grid}
         container
         spacing={0}
         alignItems="center"
-        justify="center"
+        justifyContent="center"
         style={{ height: "100vh" }}
       >
-        <Grid item xs={12} sm={4}>
+        <Grid item xs={12} sm={3} className={classes.desktop}>
           <Details title="Income" />
         </Grid>
-
-        <Grid item xs={12} sm={3}>
+        <Grid item xs={12} sm={4} className={classes.main}>
           <Main />
         </Grid>
-
-        <Grid item xs={12} sm={4}>
+        <Grid item xs={12} sm={3} className={classes.mobile}>
+          <Details title="Income" />
+        </Grid>
+        <Grid item xs={12} sm={3} className={classes.last}>
           <Details title="Expense" />
         </Grid>
       </Grid>
-    </div>
+
+      <PushToTalkButtonContainer>
+        <PushToTalkButton />
+      </PushToTalkButtonContainer>
+    </>
   );
 };
 
