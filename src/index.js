27e4719cf0b162c71b8b7027a60455b13bfdceb3
Francis Maina <sebastianfrancix@gmail.com>
2023-02-17 02:06:58 +0300

    imported speechly and added our app id

--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 import "./index.css";
+import { SpeechProvider } from "@speechly/react-client";
 
 import { Provider } from "./context/context";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  <SpeechProvider appId="f90a7634-d642-4a63-8e64-c750928c8ef6" language="en-US">
+    <Provider>
+      <App />
+    </Provider>
+  </SpeechProvider>,
+  document.getElementById("root")
+);
