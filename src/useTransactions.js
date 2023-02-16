7b8d36598b8f2b227d290bb25de00ad4e242db4f
Francis Maina <sebastianfrancix@gmail.com>
2023-02-16 10:54:15 +0300

    added array of objects for our in accordance to chart.js rules

--- src/useTransactions.js
+++ src/useTransactions.js
@@ -10,14 +10,40 @@ const useTransactions = (title) => {
   resetCategories();
   const { transactions } = useContext(ExpenseTrackerContext);
 
-  const selectedCategories = transactions.filter((t) => t.type === title);
+  const transactionsPerType = transactions.filter((t) => t.type === title);
 
-  const total = selectedCategories.reduce(
+  const total = transactionsPerType.reduce(
     (accumulator, currentValue) => (accumulator += currentValue.amount),
     0
   );
 
   const categories = title === "Income" ? incomeCategories : expenseCategories;
 
-  console.log({ selectedCategories, total, categories });
+  console.log({ transactionsPerType, total, categories });
+
+  transactionsPerType.forEach((t) => {
+    const category = categories.find((c) => c.type === t.category);
+
+    // if the category exist, increment the amount
+    if (category) category.amount += t.amount;
+  });
+
+  //   filter categories with amount = 0 (delete them) so that we can use with our charts
+  const filteredCategories = categories.filter((c) => c.amount > 0);
+
+  const chartData = {
+    // follow chart.js rules to create our chart
+    datasets: [
+      {
+        data: filteredCategories.map((c) => c.amount),
+        backgroundColor: filteredCategories.map((c) => c.color),
+      },
+    ],
+
+    labels: filteredCategories.map((c) => c.type),
+  };
+
+  return { filteredCategories, total, chartData };
 };
+
+export default useTransactions;
